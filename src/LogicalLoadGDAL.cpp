9f0128a78dee0d59f51462cedae40da884f2d528
Parker Abercrombie <parker@TheOkoriGroup.com>
2014-03-16 14:50:36 -0400

    Added top level makefile; whitespace change to LogicalLoadGDAL.cpp

--- src/LogicalLoadGDAL.cpp
+++ src/LogicalLoadGDAL.cpp
@@ -21,11 +21,9 @@ public:
     ArrayDesc inferSchema(std::vector<ArrayDesc> schemas, boost::shared_ptr<Query> query)
     {
         if (s.size() != 0) {
-            throw SYSTEM_EXCEPTION(
-                                   scidb::SCIDB_SE_INFER_SCHEMA,
+            throw SYSTEM_EXCEPTION(scidb::SCIDB_SE_INFER_SCHEMA,
                                    scidb::SCIDB_LE_ARRAY_ALREADY_EXISTS) << "input array";
         }
-
 
         return ArrayDesc();
     }
